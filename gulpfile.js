8e196d232770740ea9cf85f4c7f0b5d7b6d13726
david <dpark543107@gmail.com>
2017-01-06 16:53:58 -0600

    gulp watch sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,49 @@
-var gulp  = require('gulp'),
-    gutil = require('gulp-util');
+var gulp          = require('gulp'),
+    gutil         = require('gulp-util'),
+    sass          = require('gulp-sass'),
+    sourcemaps    = require('gulp-sourcemaps'),
+    autoprefixer  = require('gulp-autoprefixer'),
+    sassdoc       = require('sassdoc')
 
-gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
+var input         = 'src/**/*.scss'
+var output        = 'public/css'
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+var sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+gulp.task('sass', function() {
+  return gulp
+    .src(input)
+    .pipe(sourcemaps.init())
+      .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(sourcemaps.write())
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output))
+})
+
+gulp.task('sassdoc', function () {
+  return gulp
+    .src(input)
+    .pipe(sassdoc())
+    .resume();
+});
+
+gulp.task('production', ['sassdoc'], function () {
+  return gulp
+    .src(input)
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output));
+});
+
+gulp.task('watch', function() {
+  return gulp
+    .watch(input, ['sass'])
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
 });
